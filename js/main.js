b6274921c15999edc505ae618a4fdc40103fd144
yosef-w <ywolday24@gmail.com>
2023-04-02 18:22:21 -0500

    Add high and low temps

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ const search = document.getElementById('submit');
 
 search.addEventListener('click', () => {Weather(input.value)});
 
+search.addEventListener('keyup', (event) => {
+    console.log('keyup event triggered');
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        search.click()
+    }
+});
 
 async function Weather(city){
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=` + `f6cb7d4eeb654a0aafe203236230103` + `&q=` + `${city}`)
@@ -14,19 +21,27 @@ async function Weather(city){
     console.log(responseData);
     console.log(imageData);
 
-    document.querySelector('.icon').src = `http://openweathermap.org/img/wn/` + imageData.weather[0].icon + `@2x.png`
+    document.querySelector('.city').innerHTML = responseData.location.name
 
+    document.querySelector('.icon').src = `http://openweathermap.org/img/wn/` + imageData.weather[0].icon + `@2x.png`
+    
     document.querySelector('.description').innerHTML = imageData.weather[0].description
 
     document.querySelector('.temp').innerHTML = Math.round(responseData.current.temp_f) + `°`
 
-    document.querySelector('.city').innerHTML = responseData.location.name
+    document.querySelector('.high').innerHTML = Math.round((imageData.main.temp_max - 273.15) * (9/5) + 32) + `°F`;
+
+    document.querySelector('.low').innerHTML = Math.round((imageData.main.temp_min - 273.15) * (9/5) + 32) + `°F`;
+
 
     document.querySelector('.feels-like').innerHTML = Math.round(responseData.current.feelslike_f) + `°F`
 
     document.querySelector('.wind-speed').innerHTML = responseData.current.wind_mph + `mph`
 
+    document.querySelector('.information').style.display = "block";
+
     input.value = '';
+    // event.preventDefault();
 
     if (imageData.weather[0].description == "clear sky") {
         document.body.style.backgroundImage = "url('../images/am-clear-sky.jpg')";
@@ -36,7 +51,10 @@ async function Weather(city){
         document.body.style.backgroundImage = "url('../images/am-scattered-clouds.jpg')";
     } else if (imageData.weather[0].description == "broken clouds") {
         document.body.style.backgroundImage = "url('../images/am-broken-clouds.jpg')";
+    } else if (imageData.weather[0].description == "overcast clouds") {
+        document.body.style.backgroundImage = "url('../images/am-overcast-clouds.jpg')";
     }
 
+    
 
 }
